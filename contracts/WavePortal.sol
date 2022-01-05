b69ec653e87296ca49f7ac17dd1319f4983b57ef
mutugiii <mutugimutuma@gmail.com>
2022-01-05 21:48:40 +0300

    More smart contract logic && local deployment script

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,7 +5,29 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
+  uint256 totalWaves;
+  
+  struct waverData {
+    uint256 count;
+    uint256 timestamp;
+  }
+  mapping(address => waverData) waver;
+
   constructor () {
     console.log("This is the smart contract :)");
+  }
+
+  function wave() public {
+    totalWaves += 1;
+    waver[msg.sender].count += 1;
+    waver[msg.sender].timestamp = block.timestamp;
+
+    console.log("%s has waved", msg.sender);
+    console.log("%s has cumulatively waved %d time(s). The block timestamep when they waved was %s", msg.sender, waver[msg.sender].count, waver[msg.sender].timestamp);
+  }
+
+  function getTotalWaves() public view returns (uint256) {
+    console.log("We have %d total waves!", totalWaves);
+    return totalWaves;
   }
 }
